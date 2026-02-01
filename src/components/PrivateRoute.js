0b495fc12fe258d81e95bb6ebfa2ffeebf0a4c89
FSD2025 <chriswakar@gmail.com>
2026-02-01 20:45:11 +0530

    Enhance PrivateRoute to check user authentication

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,9 +3,9 @@ import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 const PrivateRoute = ({ children }) => {
-  const { token } = useContext(AuthContext);
+  const { token, user } = useContext(AuthContext);
 
-  return token ? children : <Navigate to="/login" replace />;
+  return token && user ? children : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
